69eabfe2dec858b856df28da606930ed13774d0b
John <ionutcriste28@gmail.com>
2025-02-19 21:26:46 +0000

    Add enableValidationLayers

--- constants.h
+++ constants.h
@@ -1,7 +1,16 @@
+#pragma once
 
 namespace vkp {
   const constexpr int WINDOW_WIDTH = 800;
   const constexpr int WINDOW_HEIGHT = 600;
+
+
+#ifdef NDEBUG
+  const bool enableValidationLayers = false;
+#else
+  const bool enableValidationLayers = true;
+#endif
+
 }
 
 
