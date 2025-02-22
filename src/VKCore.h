0e01ef361dd7c19dee5ade7d1fb436cc78088931
John <ionutcriste28@gmail.com>
2025-02-22 22:41:33 +0000

    Add core includes and structs

---
+++ src/VKCore.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <set>
+#include <vector>
+#include <optional>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+
+namespace vkp 
+{
+  struct QueueFamilyIndices 
+  {
+    std::optional<uint32_t> graphicsFamily;
+    std::optional<uint32_t> presentFamily;
+
+    bool IsComplete()
+    {
+      return graphicsFamily.has_value() && presentFamily.has_value();
+    }
+
+    std::set<uint32_t> ToSet() 
+    {
+      return {  
+        graphicsFamily.value(), 
+        presentFamily.value()
+      };
+    }
+  };
+
+  struct SwapChainSupportDetails 
+  {
+    VkSurfaceCapabilitiesKHR capabilities;
+    std::vector<VkSurfaceFormatKHR> formats;
+    std::vector<VkPresentModeKHR> presentModes;
+  };
+
+} 
