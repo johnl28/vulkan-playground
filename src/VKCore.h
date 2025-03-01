39aca62b585a2653459c65e98cfddfe3ebd1a71e
John <ionutcriste28@gmail.com>
2025-03-01 16:26:04 +0000

    Create Vertex struct with Binding and Attr description func

--- src/VKCore.h
+++ src/VKCore.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <set>
+#include <array>
 #include <vector>
 #include <optional>
+
+#include <glm/glm.hpp>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -36,6 +39,38 @@ namespace vkp
     VkSurfaceCapabilitiesKHR capabilities;
     std::vector<VkSurfaceFormatKHR> formats;
     std::vector<VkPresentModeKHR> presentModes;
+  };
+
+  struct Vertex 
+  {
+    glm::vec2 pos;
+    glm::vec3 color;
+
+    static VkVertexInputBindingDescription getBindingDescription() 
+    {
+      VkVertexInputBindingDescription bindingDescription{};
+      bindingDescription.binding = 0;
+      bindingDescription.stride = sizeof(Vertex);
+      bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+      return bindingDescription;
+    }
+
+    static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() 
+    {
+      std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
+      attributeDescriptions[0].binding = 0;
+      attributeDescriptions[0].location = 0;
+      attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+      attributeDescriptions[0].offset = offsetof(Vertex, pos);
+
+      attributeDescriptions[1].binding = 0;
+      attributeDescriptions[1].location = 1;
+      attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+      attributeDescriptions[1].offset = offsetof(Vertex, color);
+  
+      return attributeDescriptions;
+    }
   };
 
 } 
