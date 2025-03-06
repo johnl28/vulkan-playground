05d0bcdaf494627b7c419cb4d647e89881c2a994
John <ionutcriste28@gmail.com>
2025-03-06 20:34:19 +0000

    Add MAX_FRAMES_IN_FLIGHT constant

--- src/constants.h
+++ src/constants.h
@@ -3,13 +3,13 @@
 namespace vkp {
   constexpr int WINDOW_WIDTH = 800;
   constexpr int WINDOW_HEIGHT = 600;
+  constexpr int MAX_FRAMES_IN_FLIGHT = 2;
 
 #ifdef NDEBUG
   constexpr bool enableValidationLayers = false;
 #else
   constexpr bool enableValidationLayers = true;
 #endif
-
 }
 
 
