4c03de0f84f1ef0d5c883c66ccffc6fe9b4511b9
John <ionutcriste28@gmail.com>
2025-03-13 23:02:21 +0000

    Implement Swapchain Class Abstraction

--- src/VkpSwapchain.cpp
+++ src/VkpSwapchain.cpp
@@ -1,11 +1,15 @@
-#include "VkpSwapchain.h"
 
+#include "VkpSwapchain.h"
+#include "VkpCore.h"
+
+#include <algorithm>
 
 
 namespace vkp {
 
 
-VkpSwapchain::VkpSwapchain()
+
+VkpSwapchain::VkpSwapchain(VkSurfaceKHR _surface): surface(_surface)
 {
 
 }
@@ -15,6 +19,130 @@ VkpSwapchain::~VkpSwapchain()
 
 }
 
+void VkpSwapchain::InitSwapchain(VkPhysicalDevice physicalDevice, QueueFamilyIndices queueFamilyInidices, VkDevice device, VkExtent2D wantedExtent)
+{
+  SwapChainSupportDetails swapChainSupport = QuerySwapChainSupport(physicalDevice);
 
+  VkSurfaceFormatKHR surfaceFormat = ChooseSwapSurfaceFormat(swapChainSupport.formats);
+  VkPresentModeKHR presentMode = ChooseSwapPresentMode(swapChainSupport.presentModes);
+  VkExtent2D extent = ChooseSwapExtent(swapChainSupport.capabilities, wantedExtent);
+
+  uint32_t imageCount = swapChainSupport.capabilities.minImageCount + 1;
+  if (swapChainSupport.capabilities.maxImageCount > 0 && imageCount > swapChainSupport.capabilities.maxImageCount) 
+  {
+    imageCount = swapChainSupport.capabilities.maxImageCount;
+  }
+
+  swapChainImageFormat = surfaceFormat.format;
+  swapChainExtent = extent;
+
+  VkSwapchainCreateInfoKHR createInfo{};
+  createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+  createInfo.surface = surface;
+  createInfo.minImageCount = imageCount;
+  createInfo.imageFormat = surfaceFormat.format;
+  createInfo.imageColorSpace = surfaceFormat.colorSpace;
+  createInfo.imageExtent = extent;
+  createInfo.imageArrayLayers = 1;
+  createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+  createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+  createInfo.presentMode = presentMode;
+  createInfo.clipped = VK_TRUE;
+  createInfo.preTransform = swapChainSupport.capabilities.currentTransform;
+  createInfo.oldSwapchain = VK_NULL_HANDLE;
+
+  uint32_t queueFamilyIndicesArr[] = {queueFamilyInidices.graphicsFamily.value(), queueFamilyInidices.presentFamily.value()};
+  
+  if (queueFamilyInidices.graphicsFamily != queueFamilyInidices.presentFamily) 
+  {
+    createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+    createInfo.queueFamilyIndexCount = 2;
+    createInfo.pQueueFamilyIndices = queueFamilyIndicesArr;
+  } 
+  else 
+  {
+    createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    createInfo.queueFamilyIndexCount = 0; // Optional
+    createInfo.pQueueFamilyIndices = nullptr; // Optional
+  }
+
+  if (vkCreateSwapchainKHR(device, &createInfo, nullptr, &swapChain) != VK_SUCCESS) 
+  {
+    throw std::runtime_error("failed to create swap chain!");
+  }
+
+  vkGetSwapchainImagesKHR(device, swapChain, &imageCount, nullptr);
+  swapChainImages.resize(imageCount);
+  vkGetSwapchainImagesKHR(device, swapChain, &imageCount, swapChainImages.data());
+
+}
+
+VkSurfaceFormatKHR VkpSwapchain::ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR> &availableFormats)
+{
+  for (const auto& availableFormat : availableFormats) 
+  {
+    if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) 
+    {
+      return availableFormat;
+    }
+  }
+
+  return availableFormats[0];
+}
+
+VkPresentModeKHR VkpSwapchain::ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes) 
+{
+  for (const auto& availablePresentMode : availablePresentModes) 
+  {
+    if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) 
+    {
+      return availablePresentMode;
+    }
+  }
+
+  return VK_PRESENT_MODE_FIFO_KHR;
+}
+
+VkExtent2D VkpSwapchain::ChooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities, VkExtent2D actualExtent) 
+{
+  if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max()) 
+  {
+    return capabilities.currentExtent;
+  } 
+  else 
+  {
+    actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
+    actualExtent.height = std::clamp(actualExtent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
+
+    return actualExtent;
+  }
+}
+
+SwapChainSupportDetails VkpSwapchain::QuerySwapChainSupport(VkPhysicalDevice device)
+{
+  SwapChainSupportDetails details;
+
+  vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, &details.capabilities);
+
+  uint32_t formatCount;
+  vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, nullptr);
+  
+  if (formatCount != 0) 
+  {
+    details.formats.resize(formatCount);
+    vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, details.formats.data());
+  }
+
+  uint32_t presentModeCount;
+  vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, nullptr);
+  
+  if (presentModeCount != 0) 
+  {
+    details.presentModes.resize(presentModeCount);
+    vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, details.presentModes.data());
+  }
+
+  return details;
+}
 
 } // namespace vkp
--- src/VkpSwapchain.h
+++ src/VkpSwapchain.h
@@ -1,15 +1,36 @@
 #pragma once
+#include "vulkan/vulkan.h"
 
+#include <vector>
 
 namespace vkp {
 
   class VkpSwapchain
   {
     public:
-      VkpSwapchain();
+      VkpSwapchain(VkSurfaceKHR _surface);
       ~VkpSwapchain();
-  
-  
+
+      void InitSwapchain(VkPhysicalDevice physicalDevice, QueueFamilyIndices queueFamilyInidices, VkDevice device, VkExtent2D extent);
+
+    private:
+      SwapChainSupportDetails QuerySwapChainSupport(VkPhysicalDevice device);
+      VkSurfaceFormatKHR ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+      VkPresentModeKHR ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
+      VkExtent2D ChooseSwapExtent(const VkSurfaceCapabilitiesKHR &capabilities, VkExtent2D actualExtent);
+
+
+    private:
+      VkSurfaceKHR surface;
+
+      VkSwapchainKHR swapChain;
+
+      VkFormat swapChainImageFormat;
+      VkExtent2D swapChainExtent;
+
+      std::vector<VkFramebuffer> swapChainFramebuffers;
+      std::vector<VkImageView> swapChainImageViews;
+      std::vector<VkImage> swapChainImages;
   };
 
 }
