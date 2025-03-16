02472eebad8a581985c72a5e6d24837eee661578
John <ionutcriste28@gmail.com>
2025-03-16 21:50:10 +0000

    Use VkpFilePath type for ReadShaderFile filename argument

--- src/Utils.h
+++ src/Utils.h
@@ -3,21 +3,24 @@
 #include <vector>
 #include <fstream>
 
+#include "VkpCore.h"
+
 namespace vkp {
   class Utils
   {
     public:
-      static std::vector<char> ReadShaderFile(const std::string& filename) 
+      static std::vector<char> ReadShaderFile(const VkpFilePath& filename) 
       {
         std::ifstream file(filename, std::ios::ate | std::ios::binary);
     
         if (!file.is_open()) 
         {
-            throw std::runtime_error("failed to open file!");
+          throw std::runtime_error("Failed to open file: {}");
         }
 
         size_t fileSize = (size_t) file.tellg();
         std::vector<char> buffer(fileSize);
+
 
         file.seekg(0);
         file.read(buffer.data(), fileSize);
